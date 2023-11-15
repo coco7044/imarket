e1d3a45f291c8104c3d65f6bf59a9e89f55c4110
coco7044 <coco0118000@yahoo.co.jp>
2023-11-15 22:55:51 +0900

    category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminSeeder::class,
             ImageSeeder::class,
+            CategorySeeder::class,
         ]);
     }
 }
