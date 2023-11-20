7c287b9af41daa377b8f3b82c52e6e8b8f966dc7
coco7044 <coco0118000@yahoo.co.jp>
2023-11-20 10:24:39 +0900

    商品詳細の在庫情報

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use Illuminate\Support\Facades\DB;
+use App\Models\Stock;
 
 
 class ItemController extends Controller
@@ -55,9 +56,14 @@ class ItemController extends Controller
     public function show($id)
     {
         $product = Product::findOrFail($id);
+        $quantity = Stock::where('product_id', $product->id)
+        ->sum('quantity');
+
+        if($quantity > 9){
+            $quantity = 9;
+        }
 
         return view('user.show', 
-        compact('product'));
+        compact('product', 'quantity'));
     }
-
 }
