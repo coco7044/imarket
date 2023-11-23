8068381fa1f0d93380a05581e51ee5aa429ecf4d
coco7044 <coco0118000@yahoo.co.jp>
2023-11-23 20:32:23 +0900

    mail設定、購入完了メールと注文受付メール

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -35,9 +35,9 @@ class ItemController extends Controller
     public function index(Request $request)
     {
 
-        // 同期的に送信
-        Mail::to('test@example.com')
-        ->send(new TestMail());
+        // // 同期的に送信
+        // Mail::to('test@example.com')
+        // ->send(new TestMail());
 
         // // 非同期に送信
         // SendThanksMail::dispatch();
