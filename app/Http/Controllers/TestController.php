4c32b24a69aa942f88bb3bb2b8181058c7d3570f
coco7044 <coco0118000@yahoo.co.jp>
2023-12-23 17:31:51 +0900

    loadView作成

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -25,7 +25,7 @@ class TestController extends Controller
 
         // return view('admin.search.index',compact('backItems','geoItems'));
         
-
+        return view('admin.test.index');
         
     }
 }
