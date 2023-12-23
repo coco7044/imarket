4095ed851d25e6bb85be5fc703e2f13dd1ce8262
coco7044 <coco0118000@yahoo.co.jp>
2023-12-24 00:12:07 +0900

    cartviewレスポンシブ完成

--- resources/views/user/cart.blade.php
+++ resources/views/user/cart.blade.php
@@ -26,30 +26,30 @@
                                 <div>数量：{{ $product->pivot->quantity }}個</div>
                                 <div>価格：{{ number_format($product->pivot->quantity * $product->price )}}<span class="text-sm text-gray-700">円(税込)</span></div>
                             </div>
-                            <div class="md:w-2/12">
-                            <form method="post" action="{{route('user.cart.delete', ['item' => $product->id ])}}">
-                                @csrf
-                                <button class="flex">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                    </svg>
-                                    <span class="px-1">削除</span>
-                                </button>
-                            </form>
-                        </div>
+                            <div class="foo pb-2 md:w-2/12">
+                                <form method="post" action="{{route('user.cart.delete', ['item' => $product->id ])}}">
+                                    @csrf
+                                    <button class="flex">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                        </svg>
+                                        <span class="px-1">削除</span>
+                                    </button>
+                                </form>
+                            </div>
                         </div>
                         @endforeach
-                    <div class="my-2">
-                        小計: {{ number_format($totalPrice)}}<span class="text-sm text-gray-700">円(税込)</span>
-                    </div>
-                    <div>
-                        <button onclick="location.href='{{ route('user.cart.checkoutInfo')}}'" class="flex box ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-                            購入する
-                        </button>
-                    </div>
-                @else
-                    カートに商品が入っていません。
-                @endif
+                        <div class="py-4 foo">
+                            小計: {{ number_format($totalPrice)}}円(税込)
+                        </div>
+                        <div>
+                            <button onclick="location.href='{{ route('user.cart.checkoutInfo')}}'" class="flex box ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                                購入する
+                            </button>
+                        </div>
+                    @else
+                        カートに商品が入っていません。
+                    @endif
                 </div>
             </div>
         </div>
